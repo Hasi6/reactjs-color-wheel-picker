5c43884191d560cd089a1b0bd2a0869cc1b65fbf
Hasi6 <hasitha.chandula@gmail.com>
2022-03-03 19:28:59 +0530

    updated

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "GradientPicker", {
+  enumerable: true,
+  get: function get() {
+    return _GradientPicker.default;
+  }
+});
 exports.default = void 0;
 
 var _ColorPicker = _interopRequireDefault(require("./ColorPicker"));
+
+var _GradientPicker = _interopRequireDefault(require("./gradient/GradientPicker"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,6 @@
 import './App.css';
-import ColorPicker from 'react-color-wheel-picker';
-import { useState } from 'react';
 function App() {
-  const [colors, setColors] = useState({ hex: '#222222' });
-  return (
-    <div className='App'>
-      <ColorPicker initialColor={colors.hex} onChange={(color) => setColors(color)} size={300} />
-
-      <div>{colors.hex}</div>
-      <br />
-      {JSON.stringify(colors.rgb)}
-    </div>
-  );
+  return <div className='App'>Hasi</div>;
 }
 
 export default App;
